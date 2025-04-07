6687aa6a8c7b91b7735731a3890d2138729a577d
Jim Pravetz <jpravetz@gmail.com>
2025-04-07 15:05:00 -0600

    fix formatting in logmgr.ts and console.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { cli, Level, std } from './levels/index.ts';
 export * as Logger from './logger/index.ts';
-export { LogMgr as Mgr, type ILogMgrSettings as IMgrSettings } from './logmgr.ts';
+export { type ILogMgrSettings as IMgrSettings, LogMgr as Mgr } from './logmgr.ts';
 export * as MsgBuilder from './message/index.ts';
 export * as Transport from './transports/index.ts';
 export * from './types.ts';
