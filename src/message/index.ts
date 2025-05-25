309451d80de3bcd329fb74f92ae499f9f897b497
Jim Pravetz <jpravetz@gmail.com>
2025-05-25 10:48:00 -0600

    Export ErrOpts

--- src/message/index.ts
+++ src/message/index.ts
@@ -1,3 +1,3 @@
 export { Base } from './base.ts';
-export { Console, type IConsole } from './console.ts';
+export { Console, type ErrOpts, type IConsole } from './console.ts';
 export * from './types.ts';
