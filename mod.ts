ea37b2ef5c87aab3f72d943bbad478ac86c7417e
Jim Pravetz <jpravetz@gmail.com>
2025-07-24 17:52:57 -0600

    Regressions

--- mod.ts
+++ mod.ts
@@ -1,13 +1,1 @@
-/**
- * The `Log` module serves as the top-level namespace for the entire logging
- * system.
- *
- * @remarks
- * It exports all the core components required for logging, including the
- * {@link Log.Mgr} for management, various logger implementations, and transport
- * mechanisms. This centralized export structure simplifies imports and provides a
- * single entry point to access the library's functionalities.
- *
- * @module
- */
 export * as Log from './src/index.ts';
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
+/**
+ * The `Log` module serves as the top-level namespace for the entire logging
+ * system.
+ *
+ * @remarks
+ * It exports all the core components required for logging, including the
+ * {@link Log.Mgr} for management, various logger implementations, and transport
+ * mechanisms. This centralized export structure simplifies imports and provides a
+ * single entry point to access the library's functionalities.
+ *
+ * @module
+ */
 export { cli, Level, std } from './levels/index.ts';
 export * as Logger from './logger/index.ts';
 export { type ILogMgrSettings as IMgrSettings, LogMgr as Mgr } from './logmgr.ts';
