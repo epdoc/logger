7578061511f523c64897ab8b7c445f3bdfc2bbaa
James Pravetz <jpravetz@epdoc.com>
2026-07-27 14:02:59 -0600

    fix: correct root command options structure

--- packages/template/main.ts
+++ packages/template/main.ts
@@ -7,7 +7,7 @@ if (import.meta.main) {
   const ctx = new Ctx.Context(pkg);
   await ctx.setupLogging({ pkg: 'template' });
 
-  const cmd = new App.Cmd.Root(ctx, { root: true, dryRun: true });
+  const cmd = new App.Cmd.Root(ctx, { root: { dryRun: true } });
   await cmd.init();
 
   await CliApp.run(ctx, cmd);
